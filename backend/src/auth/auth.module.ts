7344a747547ec3ab04cd1e958a4a74024b29f850
Brayan Torres <torresacunabrayan460@gmail.com>
2023-08-16 20:43:34 -0500

    feat: Agrega variables de entorno

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,17 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtconstansts } from './constants/jwt.constants';
 
 @Module({
   imports: [
     UsersModule,
-    JwtModule.register({
-      global: true,
-      secret: jwtconstansts.secret,
-      signOptions: { expiresIn: '1d' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+        global: true,
+      }),
+      inject: [ConfigService],
     }),
   ],
   providers: [AuthService],
